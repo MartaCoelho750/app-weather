cbbd941eb6ff6a8b17fd9e20a7cd84e7b0c984a8
Marta Coelho <freelancer.martacoelho@gmail.com>
2022-08-21 19:52:58 +0100

    Update index forecast

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 4) {
+    if (index < 5 && index > 0) {
       forecastHTML =
         forecastHTML +
         `<div class="col-3">
